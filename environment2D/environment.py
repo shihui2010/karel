4252ad5f9fef5ee75c041329f46880c330298521
shihui2010 <shihui.thu@gmail.com>
2021-03-12 14:11:16 -0800

    added to-tensor methods

--- environment2D/environment.py
+++ environment2D/environment.py
@@ -1,5 +1,6 @@
 from typing import List
 from dataclasses import dataclass
+import numpy as np
 
 
 @dataclass
@@ -95,7 +96,6 @@ class Grid2D:
                     end_i, end_j = i, j
         self.move(end_i, end_j)
 
-
     def moveDown(self):
         self._arm_y = min(self._n - 1, self._arm_y + 1)
 
@@ -161,14 +161,48 @@ class Grid2D:
     def arm_loc(self):
         return self._arm_x, self._arm_y
 
+    def to_tensor(self, colors: List[str], shapes: List[str]):
+        """
+        each grid is represented as n x n x (3+k+m) tensor, where k is number of
+        colors of the markers and the m is the number of shapes
+        each cell is -d vector and
+        each element of the vector is binary indicating:
+        0: if arm on the cell
+        1: if marker present
+        2: if marker present and movable
+        {j+2}: if marker present and the color is j-th color, j=1, ..., k
+        {k+3+i: k+3+m}: if marker present and the shape is i-th shape, i=1,...,m
+        :return: np.array of shape [n, n, 3+k+m]
+        """
+        k = len(colors)
+        m = len(shapes)
+        res = np.zeros([self._n, self._n, 3 + m + k], dtype=np.int8)
+        for cid in range(self._n):
+            for rid in range(self._n):
+                if rid == self._arm_x and cid == self._arm_y:
+                    res[rid][cid][0] = 1
+                if self._grid[rid][cid] is not None:
+                    item = self._grid[rid][cid]
+                    res[rid][cid][1] = 1
+                    if item.movable:
+                        res[rid][cid][2] = 1
+                    color_id = colors.index(item.color)
+                    res[rid][cid][color_id + 3] = 1
+                    shape_id = shapes.index(item.shape)
+                    res[rid][cid][k + 3 + shape_id] = 1
+        return res
+
+
 if __name__ == "__main__":
-    env = Grid2D(n=10)
+    env = Grid2D(n=4)
     env.set_item("yellow", "round", [2, 3])
     print(env)
     print("=========")
     env.move(2, 3)
     env.pickMarker()
-    env.move(7, 6)
+    env.move(1, 1)
     env.putMarker()
-    env.move(2, 4)
+    env.move(2, 1)
     print(env)
+    res = env.to_tensor(["yellow", "red"], ["round", "square"])
+    print(res)
